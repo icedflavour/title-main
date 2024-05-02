ebcd21d190d2c985eebedc26ca56a6315b068853
Valentin <iced.flavour@gmail.com>
2024-05-02 14:36:56 +0300

    Added Header

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
-        <h2>Home Page</h2>
+        <h2 class="bg-gray-400 p-4">Home Page</h2>
     </div>
 </template>
+
+<script>
+    definePageMeta({
+        layout: 'header'
+    })
+</script>
